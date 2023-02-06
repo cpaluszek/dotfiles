fdcc853248f4b8f14b0d7497dd455a806b3a67dc
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-02-06 11:54:46 +0100

    add devicons

--- nvim/lua/noon/packer.lua
+++ nvim/lua/noon/packer.lua
@@ -6,7 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
-
+	use 'nvim-tree/nvim-web-devicons'
 	use({
 		'hardyrafael17/norminette42.nvim',
 		require("norminette").setup({
