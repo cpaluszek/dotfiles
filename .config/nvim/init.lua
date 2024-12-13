8b7841ea4b54c60dfa55d1f4593d6a1b43dfb50b
Cpaluszek <paluszek.clement@proton.me>
2024-12-13 10:13:13 +0000

    nvim: specify lazy lock path

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,7 +18,12 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+    }
+    lockfile = vim.fn.stdpath("config") .. "../lazy-lock.json",
+})
 
 local keys = require("keys")
 keys.init()
