0c8935bd716a406dd43d418a720e3f0d0f6b758c
Cpaluszek <paluszek.clement@gmail.com>
2024-12-29 18:02:52 +0100

    update fish conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,19 @@
 if status is-interactive
-    theme_gruvbox light medium
+    # theme_gruvbox light medium
+    # fish_config theme save "Catppuccin Macchiato"
     # Commands to run in interactive sessions can go here
 
     alias lg="lazygit"
     alias vim="nvim"
 
+    set fish_greeting
+
     export PATH="$PATH:/snap/bin:$HOME/.fzf/bin:$HOME/.local/bin"
+    export PATH="$PATH:/usr/local/go/bin"
 
     # Integrations
     zoxide init fish | source
     fzf --fish | source
+    starship init fish | source
 end
+
