241ef796914ed43b93b036e4c1f9a741ff66a77d
Clement PALUSZEK <cpaluszek@amiltone.com>
2025-02-26 10:38:15 +0100

    add telescope hidden files option

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,8 +9,11 @@ return {
         require("telescope").setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc="Find files" })
+        vim.keymap.set('n', '<leader>fh', function()
+            builtin.find_files( { hidden = true} )
+        end, { desc="Find files + hidden" })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {  desc="Find buffers" })
 
         -- Note: useful?
         -- git files search
