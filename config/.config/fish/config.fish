6ed6900ae4403d4219550e6a4cc9e6357cfe9c36
cpaluszek <paluszek.clement@proton.me>
2026-04-22 19:37:06 +0200

    add mise to fish config

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -12,6 +12,10 @@ if status is-interactive
     export PATH="$PATH:/snap/bin:$HOME/.fzf/bin:$HOME/.local/bin"
     export PATH="$PATH:/usr/local/go/bin:$HOME/go/bin"
 
+    # bun
+    set --export BUN_INSTALL "$HOME/.bun"
+    set --export PATH $BUN_INSTALL/bin $PATH
+
 
     if test -f "$HOME/.cargo/env.fish"
         source "$HOME/.cargo/env.fish"
@@ -21,5 +25,6 @@ if status is-interactive
     zoxide init fish | source
     fzf --fish | source
     starship init fish | source
+    mise activate | source
 end
 
